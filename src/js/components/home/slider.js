66e76f2266cb13c83fdc53a5ed138fbbeff207ab
yuwi <yuwebdev@mail.ru>
2026-03-31 12:07:54 +0300

    created testimonials section

---
+++ src/js/components/home/slider.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
+
+export const useTestimonialsSlider = () => {
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation],
+    slidesPerView: 1,
+    spaceBetween: 22,
+    loop: true,
+
+    navigation: {
+      nextEl: '.testimonials__btn--next',
+      prevEl: '.testimonials__btn--prev',
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ import '../assets/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/home/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+useTestimonialsSlider();
