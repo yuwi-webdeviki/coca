74b9b988b35928ed502449ed2461d4ed9291b185
yuwi <yuwebdev@mail.ru>
2026-04-02 01:26:43 +0300

    created pricing page. created pricing:hero

---
+++ src/js/pricing.js
@@ -0,0 +1,10 @@
+import '../assets/scss/pricing.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/price-switcher.js';
+
+useTheme();
+useBurger();
+usePriceSwitcher();
